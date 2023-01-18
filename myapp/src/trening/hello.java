4380157337d45c97596b3aef93afcef2ced171a6
HP <HP@DESKTOP-862H2AA>
2023-01-18 21:01:31 +0530

    this is asjad im sending u my fils

---
+++ myapp/src/trening/hello.java
@@ -0,0 +1,10 @@
+package trening;
+
+public class hello {
+
+	public static void main(String[] args) {
+System.out.println("welcom to the new world of devops");
+System.out.println();	}
+
+
+}
---
+++ myapp/src/trening/hi.java
@@ -0,0 +1,13 @@
+package trening;
+
+public class hi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("welcome to the new wolrd of deveops");
+System.out.println("this is my fisr project");	
+}
+	
+	}
+
+
