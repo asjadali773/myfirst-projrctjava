bcf506d179bfc17762a95315e0a7e74b873920ef
HP <HP@DESKTOP-862H2AA>
2023-01-18 21:48:09 +0530

    github intregation pass

--- myapp/src/trening/hello.java
+++ myapp/src/trening/hello.java
@@ -4,7 +4,7 @@ public class hello {
 
 	public static void main(String[] args) {
 System.out.println("welcom to the new world of devops");
-System.out.println();	}
+System.out.println("github intregration successfull");	}
 
 
 }
