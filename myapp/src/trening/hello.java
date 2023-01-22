dba8bf006f85fac6af9883498992a8e0197dce0d
HP <HP@DESKTOP-862H2AA>
2023-01-22 20:38:42 +0530

    testing automation poll scm

--- myapp/src/trening/hello.java
+++ myapp/src/trening/hello.java
@@ -4,7 +4,8 @@ public class hello {
 
 	public static void main(String[] args) {
 System.out.println("welcom to the new world of devops");
-System.out.println("github intregration successfull");	}
+System.out.println("github intregration successfull");	
+System.out.println("testing polscm");}
 
 
 }
