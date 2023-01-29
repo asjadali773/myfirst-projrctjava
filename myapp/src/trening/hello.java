214a4f2560730d489097a83e9954b0132dfe3019
HP <HP@DESKTOP-862H2AA>
2023-01-29 17:26:02 +0530

    im sending my hello java project to u

--- myapp/src/trening/hello.java
+++ myapp/src/trening/hello.java
@@ -5,7 +5,9 @@ public class hello {
 	public static void main(String[] args) {
 System.out.println("welcom to the new world of devops");
 System.out.println("github intregration successfull");	
-System.out.println("testing polscm");}
-
+System.out.println("testing pollscm");
+System.out.println("98+110");
+System.out.println("98+95");
+}
 
 }
